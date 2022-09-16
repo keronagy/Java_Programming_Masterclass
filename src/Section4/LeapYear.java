e0c835939ebc9d19ced121b48917d51e25dfcf48
DeadScripts <47615021+DeadScripts@users.noreply.github.com>
2022-09-16 17:15:45 +0300

    Adding Section 4 LeapYear

--- src/Section4/LeapYear.java
+++ src/Section4/LeapYear.java
@@ -49,12 +49,8 @@ public class LeapYear {
         if (year >= 1 && year <= 9999) {
             if (year % 4 == 0) {
                 if (year % 100 == 0) {
-                    if (year % 400 == 0) {
-                        return true;
-                    }
-                } else {
-                    return true;
-                }
+                    return year % 400 == 0;
+                } else return true;
             }
         }
         return false;
