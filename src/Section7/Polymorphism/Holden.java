ff30d2d03dc569bfcceb9f757059558591a82e0f
DeadScripts <47615021+DeadScripts@users.noreply.github.com>
2022-09-25 01:37:44 +0300

    Adding Section 7 Polymorphism

---
+++ src/Section7/Polymorphism/Holden.java
@@ -0,0 +1,24 @@
+package Section7.Polymorphism;
+
+public class Holden extends Car{
+
+    public Holden(int cylinders, String name) {
+        super(cylinders, name);
+    }
+
+    @Override
+    public String startEngine() {
+        return ("Holden -> startEngine()");
+    }
+
+    @Override
+    public String  accelerate() {
+        return ("Holden -> accelerate()");
+    }
+
+    @Override
+    public String  brake() {
+        return ("Holden -> brake()");
+    }
+
+}
